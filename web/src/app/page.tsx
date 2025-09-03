7c7aa2ed0a85cf4e5d7e889c395ddde2b63bfe0e
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2025-09-03 16:51:52 +0100

    slide in animations and socials at the bottom

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,10 +1,20 @@
+"use client";
+
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 import './home.css'
+import { useEffect } from 'react';
+import { Instagram, Mail, Phone } from 'lucide-react';
 
 export default function Home() {
+  useEffect(() => {
+    Aos.init();
+  }, []);
+
   return (
-    <div className="font-sans flex items-center justify-items-center min-h-screen p-8 pb-20 gap-16">
-      <main className="flex flex-col gap-[32px] items-center">
-        <h3 className="home-title">Abel BarberShop</h3>
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen px-4 pb-20 gap-16">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center">
+        <h3 data-aos="fade-right" data-aos-duration="900" className="home-title">Abel BarberShop</h3>
         <div id="pole">
           <div className="dome top"></div>
           <div className="ring top"></div>
@@ -18,11 +28,30 @@ export default function Home() {
           <div className="ring bottom"></div>
           <div id="shadow"></div>
         </div>
-        <p className="px-4 text-center">Fresh cuts, good vibes, and no waiting around. Book your time and you’re set.</p>
-        <div className="px-4">
-          <button className="bg-[#FE5F55] px-3 py-1.5 rounded-lg font-sans">Book Your Appointment</button>
+        <p data-aos="fade-right" data-aos-duration="900" className="px-4 text-center">Fresh cuts, good vibes, and no waiting around. Book your time and you’re set.</p>
+        <div data-aos="fade-right" data-aos-duration="900" className="holographic-container">
+          <div className="holographic-card">
+            <h2>Book Your Appointment</h2>
+          </div>
         </div>
       </main>
+      <footer data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-duration="900"  className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a className="flex items-center gap-2 hover:underline hover:underline-offset-4" 
+          href="https://google.com" target="_blank" rel="noopener noreferrer"
+        >
+          <Instagram color='white' />Instagram
+        </a>
+        <a className="flex items-center gap-2 hover:underline hover:underline-offset-4" 
+          href="https://google.com" target="_blank" rel="noopener noreferrer"
+        >
+          <Phone color='white' />Phone
+        </a>
+        <a className="flex items-center gap-2 hover:underline hover:underline-offset-4" 
+          href="https://google.com" target="_blank" rel="noopener noreferrer"
+        >
+          <Mail color='white' />Email
+        </a>
+      </footer>
     </div>
   );
 }
