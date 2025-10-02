8e690eec0e4077c8a15b95b0b258813d65191933
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2025-10-02 10:00:15 +0100

    create application runner

---
+++ services/runner/src/main/java/com/booking/system/ApplicationRunner.java
@@ -0,0 +1,12 @@
+package com.booking.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.booking.system")
+public class ApplicationRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationRunner.class, args);
+    }
+}
