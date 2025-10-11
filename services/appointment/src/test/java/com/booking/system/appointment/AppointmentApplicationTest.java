7a1ce2d65a55db493f1452597cb96a32211c8452
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2025-10-11 18:25:36 +0100

    Update sonar properties & add jacoco for submodules coverage

---
+++ services/appointment/src/test/java/com/booking/system/appointment/AppointmentApplicationTest.java
@@ -0,0 +1,10 @@
+package com.booking.system.appointment;
+
+import org.junit.jupiter.api.Test;
+
+class AppointmentApplicationTest {
+    @Test
+    public void runMain() {
+        AppointmentApplication.main(new String[] {});
+    }
+}
