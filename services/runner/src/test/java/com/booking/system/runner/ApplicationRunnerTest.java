9fcc08948d2e55675f9241efdf1ff18a9d1e2f76
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2025-10-11 17:37:53 +0100

    Application Runner App Test

---
+++ services/runner/src/test/java/com/booking/system/runner/ApplicationRunnerTest.java
@@ -0,0 +1,10 @@
+package com.booking.system.runner;
+
+import org.junit.jupiter.api.Test;
+
+class ApplicationRunnerTest {
+    @Test
+    public void runMain() {
+        ApplicationRunner.main(new String[] {});
+    }
+}
